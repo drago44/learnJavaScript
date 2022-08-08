3622c89f74c06c714df29c767692f00f963515cb
Alex <oleksander.kucherenko.work@gmail.com>
2022-08-08 21:18:34 +0300

    cycles and conditions

--- 00/script.js
+++ 00/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const numberOfFilms = +prompt('Скількі  фільмів ви подивились', '');
 
 const personalMovieDB = {
@@ -8,12 +10,30 @@ const personalMovieDB = {
   privat: false
 }
 
-const a = prompt('Який останній фільм ви подивились', '');
-const b = prompt('Як ви оцінюєте останній переглянутий фільм', '');
-const c = prompt('Який останній фільм ви подивились', '');
-const d = prompt('Як ви оцінюєте останній переглянутий фільм', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+  const a = prompt('Який останній фільм ви подивились', '');
+  const b = prompt('Як ви оцінюєте останній переглянутий фільм', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10){
+  console.log('мало');
+} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30 ) {
+  console.log('добре');
+} else if (personalMovieDB.count >= 30){
+  console.log('кіноман');
+} else {
+  console.log('помилка');
+}
+
+
 
 console.log(personalMovieDB);
