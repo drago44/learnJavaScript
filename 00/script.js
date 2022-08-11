03cb7c83692809761d6d74365f7a8b7eb7588975
Alex <oleksander.kucherenko.work@gmail.com>
2022-08-11 18:09:33 +0300

    delete advertising, changed genre

--- 00/script.js
+++ 00/script.js
@@ -1,23 +1,23 @@
 "use strict"
 
 const personalMovieDB = {
-  count: 0, 
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
-  start: function() {
+  start: function () {
     personalMovieDB.count = +prompt('Скількі  фільмів ви подивились', '');
-  
+
     while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-    personalMovieDB.count = +prompt('Скількі  фільмів ви подивились', '');
+      personalMovieDB.count = +prompt('Скількі  фільмів ви подивились', '');
     }
   },
-  rememberMyFilms: function() {
-    for (let i = 0; i < 2; i++){
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
       const a = prompt('Який останній фільм ви подивились', '').trim();
       const b = prompt('Як ви оцінюєте останній переглянутий фільм', '');
-    
+
       if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
         console.log('done');
@@ -27,33 +27,33 @@ const personalMovieDB = {
       }
     }
   },
-  detectPersonalLevel: function() {
-    if (personalMovieDB.count < 10){
+  detectPersonalLevel: function () {
+    if (personalMovieDB.count < 10) {
       console.log('мало');
-    } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30 ) {
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
       console.log('добре');
-    } else if (personalMovieDB.count >= 30){
+    } else if (personalMovieDB.count >= 30) {
       console.log('кіноман');
     } else {
       console.log('помилка');
     }
   },
-  showMyDB: function(hidden) {
+  showMyDB: function (hidden) {
     if (!hidden) {
       console.log(personalMovieDB);
     } else {
       console.log('DB is privat')
     }
   },
-  toggleVisibleMyDB: function() {
-    if (personalMovieDB.privat ){
+  toggleVisibleMyDB: function () {
+    if (personalMovieDB.privat) {
       personalMovieDB.privat = false;
     } else {
       personalMovieDB.privat = true;
     }
   },
-  writeYourGenres: function() {
-    for (let i = 1; i <= 3; i++){
+  writeYourGenres: function () {
+    for (let i = 1; i <= 3; i++) {
       let genres = prompt(`Ваш улюблений жанр під номером ${i}`).trim().toLowerCase();
 
       if (genres == null || genres == '') {
