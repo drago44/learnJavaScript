da780db885679a3e3c6db79d4d02fc2f9c8640d6
Alex <oleksander.kucherenko.work@gmail.com>
2022-08-11 18:41:48 +0300

    chanched poster, sort and made dynamic movie list

--- 01/js/script.js
+++ 01/js/script.js
@@ -3,7 +3,7 @@
 const movieDB = {
   movies: [
     "Логан",
-    "Лига справедливости",
+    "Апокалипсис сегодня ",
     "Ла-ла лэнд",
     "Одержимость",
     "Скотт Пилигрим против..."
@@ -11,10 +11,26 @@ const movieDB = {
 };
 
 const adv = document.querySelectorAll('.promo__adv img');
-const genre = document.querySelector('.promo__genre');
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(item => {
   item.remove();
 });
 
 genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+      <div class="delete"></div>
+    </li>
+  `
+});
