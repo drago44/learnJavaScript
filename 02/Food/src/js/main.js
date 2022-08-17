fc6959da94402719dea690d49544342893eb1407
Alex <oleksander.kucherenko.work@gmail.com>
2022-08-17 19:25:00 +0300

    new project 02-Food made tabs

---
+++ 02/Food/src/js/main.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const tab = document.querySelectorAll('.tabheader__item');
+  const tabContent = document.querySelectorAll('.tabcontent');
+  const tabParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+
+    tabContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tab.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabContent[i].classList.add('show', 'fade');
+    tabContent[i].classList.remove('hide');
+    tab[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tab.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+});
