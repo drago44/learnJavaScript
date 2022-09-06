8d0659943ac41af9c4c07a50b7f18374d8ed9e14
Alex <oleksander.kucherenko.work@gmail.com>
2022-09-06 16:01:21 +0300

    made render methid for menu card

--- 02/Food/src/js/main.js
+++ 02/Food/src/js/main.js
@@ -155,13 +155,43 @@ document.addEventListener('DOMContentLoaded', () => {
   //Make classes for cards
 
   class MenuCard {
-    constructor(src, alt, title, desc, price) {
+    constructor(src, alt, title, desc, price, parentSelector) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.desc = desc;
       this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 35;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+        <div class="menu__item">
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.desc}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
+        </div>
+      `;
+      this.parent.append(element);
     }
   }
+
+  // first example
+  // const div = new MenuCard();
+  // div.render();
+
+  new MenuCard().render;
 
 });
